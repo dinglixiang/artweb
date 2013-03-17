60dc467ce7d04464f0ff106ead4c812d6ab4bf77
丁历祥 <dlx_swpu@qq.com>
2013-03-17 14:21:33 +0800

    create some static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Artweb::Application.routes.draw do
+  root :to => 'static_pages#home'
+
+  #match '/home', to: "static_pages#home"
+  match '/rule', to: "static_pages#rule"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Artweb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
