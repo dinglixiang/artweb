549ae044ac755ad80c13916a33bc70881232c8d0
丁历祥 <dlx_swpu@qq.com>
2013-03-17 13:02:59 +0800

    Initial artweb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artweb::Application.config.secret_token = 'fdeea0b847a566cfce10dab9711a807ca2f766d1c82dfe8ecc157d7ed6b2f35133ab199b040372f220533cce548740d140e8371634848b09a132f2e862931d29'
